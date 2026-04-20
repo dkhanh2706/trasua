e2ab747fe97d1034ea978ee0591fee0413a5badb
khanh25000 <khanhbin281323@gmail.com>
2026-04-20 12:15:03 +0700

    fix

--- app.py
+++ app.py
@@ -928,3 +928,9 @@ def admin_logout():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
